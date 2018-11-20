081664e44f784e798050fd6ebe5fb111af81d0b9
Bartosz Przygocki <tazer@o2.pl>
2018-11-20 17:38:06 +0100

    Last ver

--- Multiple Rergression R.R
+++ Multiple Rergression R.R
@@ -73,11 +73,11 @@ pred_profile<-rfe (existing_out[,predictorsVar],existing_out[,outcomeVar],rfeCon
 pred_profile
 
 #Build the correlation matrix
-corr_existing_out <- cor(existing_out[,2:16]) #dont include product type
+corr_existing_out <- cor(existing_out[,2:15]) #dont include product type
 corr_existing_out
 #nice plot of the correlation matrix
-pMat2 <- cor.mtest(existing_out, conf.level = .95)#########################for later remove product type 
-corrplot(corr_existing_out,order="hclust",method="number",)
+pMat2 <- cor.mtest(corr conf.level = .95)#########################for later remove product type 
+corrplot(corr_existing_out,order="hclust",method="number",p.mat = pmat2)
 ##################################################################################################
 
 
@@ -86,7 +86,7 @@ corrplot(corr_existing_out,order="hclust",method="number",)
 #5star,profit margin, 3star, 2star, 1star, negative service review, width and height
 existing_out$x5StarReviews <- NULL
 existing_out
-existing_out_less <- subset(existing_out,select=c(x4StarReviews,PositiveServiceReview,Volume))
+existing_out_less <- subset(existing_out,select=c(x4StarReviews,PositiveServiceReview,Volume,ShippingWeight,Price,ProductDepth))
 existing_out_less
 #if ovverfitted we remove positive revew 
 #Anova to see if the product type is related to volume
@@ -122,8 +122,6 @@ varImpPlot(fit_rf,type=2)
 #existing_dum_PLNS <- existing_dum[,13:20]
 
 #################################################################################################
-treedummy <-ctree(Volume~., existing_dum, control = ctree_control(maxdepth = 6))
-plot(treedummy)
 
 treenodummy <-ctree(Volume~.-x5StarReviews,existing,control= ctree_control(maxdepth = 6))
 plot(treenodummy)
@@ -158,79 +156,137 @@ boxplot(testing$Volume, main="Testing")
 plot(training$Volume, main="Training")
 plot(testing$Volume, main="Testing")
 ##########Here to start the models##########################################################################
+#Train and Tune the SVM
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+#change this to a randome range maybe like gamma = 10^(-5:-1), cost = 10^(-3:1))
+
+
+
+# Use the expand.grid to specify the search space	
+SVMgrid <- expand.grid(sigma = c(.01, .015, 0.2),
+                                   C = c(0.75, 0.9, 1, 1.1, 1.25)
+#for loop for atrubites maybe would be nice  
+                       
+svm_modelAll <- train(Volume ~., 
+                      data = training, 
+                      method="svmRadial", 
+                      trControl=trctrl,
+                      tuneGrid = SVMgrid,
+                      preProc = c("scale","center"),
+                      metric="RMSE",
+                      verbose=FALSE)
+
+svm_modelOne <- train(Volume ~x4StarReviews, 
+                      data = training, 
+                      method="svmRadial", 
+                      trControl=trctrl,
+                      tuneGrid = SVMgrid,
+                      preProc = c("scale","center"),
+                      metric="RMSE",
+                      verbose=FALSE)
+
+svm_modelTwo <- train(Volume ~x4StarReviews + PositiveServiceReview, 
+                      data = training, 
+                      method="svmRadial", 
+                      trControl=trctrl,
+                      tuneGrid = SVMgrid,
+                      preProc = c("scale","center"),
+                      metric= "RMSE",
+                      verbose=FALSE)
+
+
+
+svm_modelThree <- train(Volume ~x4StarReviews + PositiveServiceReview + ShippingWeight, 
+                        data = training, 
+                        method="svmRadial", 
+                        trControl=trctrl,
+                        tuneGrid = SVMgrid,
+                        preProc = c("scale","center"),
+                        metric= "RMSE",
+                        verbose=FALSE)
+
+svm_modelFour<- train(Volume ~x4StarReviews + PositiveServiceReview + ShippingWeight +Price, 
+                       data = training, 
+                       method="svmRadial", 
+                       trControl=trctrl,
+                       tuneGrid = SVMgrid,
+                       preProc = c("scale","center"),
+                       metric= "RMSE",
+                       verbose=FALSE)
+
+svm_modelFife <- train(Volume ~x4StarReviews + PositiveServiceReview + ShippingWeight +Price +ProductDepth, 
+                       data = training, 
+                       method="svmRadial", 
+                       trControl=trctrl,
+                       tuneGrid = SVMgrid,
+                       preProc = c("scale","center"),
+                       metric= "RMSE",
+                       verbose=FALSE)
 
 
 
 
+rValues <- resamples(list(svm_modelAll,svm_modelOne,svm_modelTwo,svm_modelThree,svm_modelFour,svm_modelFife))
+rValues$values
+
+summary(rValues)
+bwplot(rValues,metric="RMSE",ylab =c("radial kernel"),main="SVM Models")
 
 
-
-
-str(existing_dum_PLNS)
-
-#predSCM <- predict(svm_model,newdata = testing)
-#predSCM1 <- predict(svm_model1,newdata = testing)
-#predSCM2 <- predict(svm_model2,newdata = testing)
-#predSCM3 <- predict(svm_model3,newdata = testing)
-#predSCM4 <- predict(svm_model4,newdata = testing)
-#predSCM5 <- predict(svm_model5,newdata = testing)
-
-
-
-tuned_parametersPLNS <- tune.svm(Volume ~ PositiveServiceReview, data = training, gamma = 10^(-5:-1), cost = 10^(-3:1))
-#tuned_parameters1PLNS <- tune.svm(Volume ~ PositiveServiceReview, data = training, gamma = 10^(-5:-1), cost = 10^(-3:1))
-#tuned_parameters2PLNS <- tune.svm(Volume ~ x4StarReviews , data = training, gamma = 10^(-5:-1), cost = 10^(-3:1))
-#tuned_parameters3PLNS <- tune.svm(Volume ~ PositiveServiceReview +  x4StarReviews +ProductType.PLNS +ProductDepth,data = training, gamma = 10^(-5:-1), cost = 10^(-3:1))
-#tuned_parameters4PLNS <- tune.svm(Volume ~ PositiveServiceReview +  x4StarReviews +ProductType.PLNS +ProductDepth +Recommendproduct, data = training, gamma = 10^(-5:-1), cost = 10^(-3:1))
-
-#kernel "linear" might be overffitng 
-
-summary(tuned_parametersPLNS)
-#summary(tuned_parameters1PLNS )
-#summary(tuned_parameters2PLNS )
-#summary(tuned_parameters3PLNS )
-#summary(tuned_parameters4PLNS )
-
-#Models 
-svm_modelPLNS <- svm(Volume ~ ., data=training,kernel="linear", gamma = 0.1, cost = 10,preProcess= c("Center","Scale"),metric="RMSE")
-svm_model1PLNS <- svm(Volume ~ PositiveServiceReview, data=training,kernel="radial", gamma = 0.1, cost = 10)
-svm_model2PLNS <- svm(Volume ~ PositiveServiceReview +  x4StarReviews, data=training,kernel="radial", gamma = 0.1, cost = 10)
-svm_model3PLNS <- svm(Volume ~ PositiveServiceReview +  x4StarReviews +ProductType.PLNS, data=training,kernel="radial", gamma = 0.1, cost = 10)
-svm_model4PLNS <- svm(Volume ~ PositiveServiceReview +  x4StarReviews +ProductType.PLNS +ProductDepth, data=training,kernel="radial", gamma = 0.1, cost = 10)
-svm_model5PLNS <- svm(Volume ~ PositiveServiceReview +  x4StarReviews +ProductType.PLNS +ProductDepth +Recommendproduct, data=training,kernel="radial", gamma = 0.1, cost = 10)
-
-
-svm_modelPLNS
 
 #> my_prediction <- predict(my_model, test_set[,-1])
-#test_set[,-1] removes the first column (the class column) to make the predictions only based on the features of the data. You should remove the column that labels your data.
+#test_set[,-1] removes  ???? the first column (the class column) to make the predictions only based on the features of the data. You should remove the column that labels your data.
 
 
-predSVMPLNS <- predict(svm_modelPLNS,newdata = testing)
-predSVM1PLNS <- predict(svm_model1PLNS,newdata = testing)
-predSVM2PLNS <- predict(svm_model2PLNS,newdata = testing)
-predSVM3PLNS <- predict(svm_model3PLNS,newdata = testing)
-predSVM4PLNS <- predict(svm_model4PLNS,newdata = testing)
-predSVM5PLNS <- predict(svm_model5PLNS,newdata = testing)
-
-#why this is not working 
+predSVMall <- predict(svm_modelAll,newdata = testing)
+predSVMOne <- predict(svm_modelOne,newdata = testing)
+predSVMtwo <- predict(svm_modelTwo,newdata = testing)
+predSVMThree <- predict(svm_modelThree,newdata = testing)
+predSVMFour <- predict(svm_modelFour,newdata = testing)
+predSVMFife <- predict(svm_modelFife,newdata = testing)
 
 
+
+
+
+
+
+
+
+
+
+predSVMOne
 par(mfrow=c(1,1))
 
-plot(predSVMPLNS,testing$Volume)
+plot(predSVMFife,testing$Volume)
 abline(a=0,b=1,col="blue")
 
-postResample(predSVMPLNS,testing$Volume)
-rmse(testing$Volume,predSVMPLNS)
+postResample(predSVMall,testing$Volume)
+postResample(predSVMOne,testing$Volume)
+postResample(predSVMtwo,testing$Volume)
+postResample(predSVMThree,testing$Volume)
+postResample(predSVMFour,testing$Volume)
+postResample(predSVMFife,testing$Volume)
+
 
 
 #realtive error read about it 
-realErro<- (predSVMPLNS -testing$Volume)/testing$Volume  # to calc % 
-realErro
+realErroall<- (predSVMall -testing$Volume)/testing$Volume  # to calc % 
+realErroOne<- (predSVMOne-testing$Volume)/testing$Volume 
+realErroTwo<- (predSVMtwo -testing$Volume)/testing$Volume 
+realErroThree<- (predSVMThree -testing$Volume)/testing$Volume 
+realErroFour<- (predSVMFour -testing$Volume)/testing$Volume 
+realErroFive<- (predSVMFife -testing$Volume)/testing$Volume 
 
-mean(realErro)
-
+mean(realErroall)
+mean(realErroOne)
+mean(realErroTwo)
+mean(realErroThree)
+mean(realErroFour)
+mean(realErroFive)
+pal=c("lightblue","mistyrose")
+plot(predSVMThree,testing$Volume)
+abline(a=0,b=1,col="mistyrose")
 
 
 
